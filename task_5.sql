db7ba7007eefaea7d91b79a72485c8eb16fa83d3
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-10-31 10:42:28 +0300

    Add  task 5 inserting data

---
+++ task_5.sql
@@ -0,0 +1,10 @@
+import mysql.connector
+alxdb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="and324292and",
+    database="alx_book_store"
+)
+alxcursor = alxdb.cursor()
+alxcursor.execute("INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '124 Happiness Ave.');")
+alxdb.commit()
